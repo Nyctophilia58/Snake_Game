8d85d5dc79af236023dd9ab21e2bf9e0ef7007df
Nyctophilia58 <nowshingdr@protonmail.com>
2022-10-20 21:56:37 +0600

    commit

--- image_resizing.py
+++ image_resizing.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-image = Image.open("Pictures/background.png")
-new_image = image.resize((900, 900))
-new_image.save('Pictures/background.png')
+image = Image.open("Pictures/Git.png")
+new_image = image.resize((60, 60))
+new_image.save('Pictures/Git.png')
 new_image.show()
