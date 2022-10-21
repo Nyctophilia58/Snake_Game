6e878eb409aee08b52486aae2c940a94d5db0c9f
Nyctophilia58 <nowshingdr@protonmail.com>
2022-10-21 11:15:25 +0600

    commit

--- image_resizing.py
+++ image_resizing.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-image = Image.open("Pictures/game_screen.png")
+image = Image.open("Pictures/collide.png")
 new_image = image.resize((700, 700))
-new_image.save('Pictures/game_screen.png')
+new_image.save('Pictures/collide.png')
 new_image.show()
